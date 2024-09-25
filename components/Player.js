5a991e15c75f0b5dc81b48335d0d15daaf41d19d
金河 <myter7@gmail.com>
2024-09-25 19:38:05 +0800

    update music

--- components/Player.js
+++ components/Player.js
@@ -72,10 +72,7 @@ const Player = () => {
           type='playlist'
           preload='auto'
           lrc-type={siteConfig('MUSIC_PLAYER_METING_LRC_TYPE')}
-          // api={siteConfig(
-          //   'MUSIC_PLAYER_METING_API',
-          //   'https://api.i-meto.com/meting/api'
-          // )}
+          api={siteConfig('MUSIC_PLAYER_METING_API')}
           autoplay={autoPlay}
           order={siteConfig('MUSIC_PLAYER_ORDER')}
           server={siteConfig('MUSIC_PLAYER_METING_SERVER')}
