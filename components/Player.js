81e05ed29549c294be9abee8dd2000909a736541
金河 <myter7@gmail.com>
2024-09-25 19:52:03 +0800

    update

--- components/Player.js
+++ components/Player.js
@@ -72,10 +72,7 @@ const Player = () => {
           type='playlist'
           preload='auto'
           lrc-type={siteConfig('MUSIC_PLAYER_METING_LRC_TYPE')}
-          api={siteConfig(
-            'MUSIC_PLAYER_METING_API',
-            'https://api.injahow.cn/meting'
-          )}
+          api='https://api.injahow.cn/meting/?server=tencent&type=playlist&id=9330992009'
           autoplay={autoPlay}
           order={siteConfig('MUSIC_PLAYER_ORDER')}
           server={siteConfig('MUSIC_PLAYER_METING_SERVER')}
